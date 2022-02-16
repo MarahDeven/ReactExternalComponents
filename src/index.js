df9172e1b9749baa35636284579423d776b885c8
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-16 10:07:28 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React External Components</h1>
+
+      <h3>Challenge 1</h3>
+      <p>
+        Import and render this component{" "}
+        <a href="https://www.npmjs.com/package/react-animated-weather">
+          https://www.npmjs.com/package/react-animated-weather
+        </a>
+      </p>
+
+      <br />
+      <h3>Challenge 2</h3>
+      <p>
+        Display 3 different icons and change the properties for each of them{" "}
+      </p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
